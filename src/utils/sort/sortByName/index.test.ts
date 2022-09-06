dc8e76e271d89ceeae8de144d6c665ac48bc764b
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2022-09-06 09:44:27 +0300

    [EPMDEDP-10546]: fix: Fixes headlamp's sortByName function crash
    Change-Id: I03526b3d52b52085026b74dcf9fa2c4138c8d40e

--- src/utils/sort/sortByName/index.test.ts
+++ src/utils/sort/sortByName/index.test.ts
@@ -2,15 +2,17 @@ import { sortByName } from './index';
 
 describe('checking sortByName', () => {
     test('checking with no-accent chars', () => {
-        const testArray = ['Banana', 'Orange', 'Apple', 'Mango'];
+        const testArray = ['Banana', 'Orange', '', undefined, 'Apple', 'Mango'];
         const sortedArray = testArray.sort(sortByName);
 
-        const [item1, item2, item3, item4] = sortedArray;
+        const [item1, item2, item3, item4, item5, item6] = sortedArray;
 
         expect(item1).toMatch('Apple');
         expect(item2).toMatch('Banana');
         expect(item3).toMatch('Mango');
         expect(item4).toMatch('Orange');
+        expect(item5).toBe('');
+        expect(item6).toBeUndefined();
     });
 
     test('checking with accent chars', () => {
--- src/utils/sort/sortByName/index.ts
+++ src/utils/sort/sortByName/index.ts
@@ -1,2 +1,15 @@
-export const sortByName = (a: string, b: string): number =>
-    a.localeCompare(b, undefined, { sensitivity: 'accent' });
+export const sortByName = (a?: string, b?: string): number => {
+    if (a && b) {
+        return a.localeCompare(b, undefined, { sensitivity: 'accent' });
+    }
+
+    if (a && !b) {
+        return -1;
+    }
+
+    if (!a && b) {
+        return 1;
+    }
+
+    return 0;
+};
