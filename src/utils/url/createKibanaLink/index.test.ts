47cd92c2e05c6d76877decad0811ba478fa2ec0d
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2023-06-15 17:11:08 +0300

    [EPMDEDP-12151]: feat: Update kibana link creation template
    Change-Id: Ie98097a3d635db411e97d93339639dff295ebb22

--- src/utils/url/createKibanaLink/index.test.ts
+++ src/utils/url/createKibanaLink/index.test.ts
@@ -10,7 +10,7 @@ describe('test createKibanaLink util', () => {
                 'test-namespace'
             )
         ).toEqual(
-            "https://kibana.eks-sandbox.aws.main.edp.projects.epam.com/app/discover#/?_g=()&_a=(columns:!(),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes_namespace_name,negate:!f,params:(query:test-namespace),type:phrase),query:(match_phrase:(kubernetes_namespace_name:test-namespace)))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))"
+            "https://kibana.eks-sandbox.aws.main.edp.projects.epam.com/app/discover#/?_g=()&_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes_namespace_name,negate:!f,params:(query:test-namespace),type:phrase),query:(match_phrase:(kubernetes_namespace_name:test-namespace)))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))"
         );
     });
 });
--- src/utils/url/createKibanaLink/index.ts
+++ src/utils/url/createKibanaLink/index.ts
@@ -7,5 +7,5 @@ export const createKibanaLink = (EDPComponentsURLS: EDPComponentsURLS, namespace
         return;
     }
 
-    return `${kibanaURLOrigin}/app/discover#/?_g=()&_a=(columns:!(),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes_namespace_name,negate:!f,params:(query:${namespace}),type:phrase),query:(match_phrase:(kubernetes_namespace_name:${namespace})))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))`;
+    return `${kibanaURLOrigin}/app/discover#/?_g=()&_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes_namespace_name,negate:!f,params:(query:${namespace}),type:phrase),query:(match_phrase:(kubernetes_namespace_name:${namespace})))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))`;
 };
