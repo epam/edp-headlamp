53ec04de4d77a0e464833a1ee1583ff604be5985
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2023-07-03 15:48:25 +0300

    [EPMDEDP-11651]: feat: Make status tooltip interactive
    Change-Id: If3ece375a873f96c329911ee3cd1b09dfac675d1

--- src/components/StatusIcon/index.tsx
+++ src/components/StatusIcon/index.tsx
@@ -8,14 +8,17 @@ import { StatusIconProps } from './types';
 const { Icon } = Iconify;
 const { Tooltip } = MuiCore;
 
+const stopPropagation = (e: React.SyntheticEvent) => e.stopPropagation();
+
 export const StatusIcon = ({ status, customTitle, width = 25 }: StatusIconProps) => {
     const classes = useStyles();
 
     const [icon, color, animate] = getCustomResourceStatusIconByStatusName(status);
 
     return (
-        <div>
-            <Tooltip title={customTitle ?? capitalizeFirstLetter(status)}>
+        // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
+        <div onClick={stopPropagation}>
+            <Tooltip title={customTitle ?? capitalizeFirstLetter(status)} interactive>
                 <Icon
                     icon={icon}
                     color={color}
