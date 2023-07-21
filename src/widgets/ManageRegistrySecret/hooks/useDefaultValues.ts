6508eec1c5b61137febdf5160652d7121d9b927c
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2023-07-21 12:44:05 +0300

    [EPMDEDP-12241]: feat: Update ManageRegistrySecret widget registryEndpoint default value
    Change-Id: I6367fcbaf8da28ee8c0f10344e37082c1404d62a

--- src/widgets/ManageRegistrySecret/hooks/useDefaultValues.ts
+++ src/widgets/ManageRegistrySecret/hooks/useDefaultValues.ts
@@ -2,30 +2,54 @@ import React from 'react';
 import { REGISTRY_SECRET_CREATION_FORM_NAMES } from '../names';
 import { ManageRegistrySecretFormDataContext } from '../types';
 
+const parseConfigJson = (configJson: string) => {
+    const decodedConfigJson = atob(unescape(configJson));
+    return JSON.parse(decodedConfigJson);
+};
+
 export const useDefaultValues = ({
     formData,
 }: {
     formData: ManageRegistrySecretFormDataContext;
 }) => {
-    const { currentElement, registryEndpoint } = formData;
+    const { currentElement, registryEndpoint: dockerRegistryEndpoint } = formData;
 
     const getUserNameAndPassword = React.useCallback(() => {
         if (typeof currentElement === 'string' && currentElement === 'placeholder') {
             return { userName: undefined, password: undefined };
         }
         const configJson = currentElement?.data?.['.dockerconfigjson'];
+
         if (configJson) {
-            const decodedConfigJson = atob(unescape(configJson));
-            const parsedConfigJson = JSON.parse(decodedConfigJson);
+            const parsedConfigJson = parseConfigJson(configJson);
             // @ts-ignore
             const userName = Object.values(parsedConfigJson.auths)[0]?.username;
             // @ts-ignore
             const password = Object.values(parsedConfigJson.auths)[0]?.password;
             return { userName, password };
+        } else {
+            return { userName: undefined, password: undefined };
         }
     }, [currentElement]);
 
+    const getRegistryEndpoint = React.useCallback(() => {
+        if (typeof currentElement === 'string' && currentElement === 'placeholder') {
+            return dockerRegistryEndpoint;
+        }
+        const configJson = currentElement?.data?.['.dockerconfigjson'];
+        if (configJson) {
+            const parsedConfigJson = parseConfigJson(configJson);
+            // @ts-ignore
+            const endpoint = Object.keys(parsedConfigJson?.auths)?.[0];
+
+            return endpoint || dockerRegistryEndpoint;
+        } else {
+            return dockerRegistryEndpoint;
+        }
+    }, [currentElement, dockerRegistryEndpoint]);
+
     const { userName, password } = getUserNameAndPassword();
+    const registryEndpoint = getRegistryEndpoint();
 
     return React.useMemo(() => {
         return {
