07a680c59c8d016fbe684de83ebfc091c0428714
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2023-10-31 11:43:55 +0200

    feat: Disable Cluster Name field for edit in cluster secret creation widget  (#82)
    Jira: EPMDPEDP-12815
    Related: #82
    Change-Id: I4fa0e7431b6ea5f1bc5bd98746185491900c0075

--- src/widgets/ManageClusterSecret/components/fields/ClusterName/index.tsx
+++ src/widgets/ManageClusterSecret/components/fields/ClusterName/index.tsx
@@ -2,8 +2,10 @@ import { Grid } from '@material-ui/core';
 import React from 'react';
 import { useFormContext as useReactHookFormContext } from 'react-hook-form';
 import { FormTextField } from '../../../../../providers/Form/components/FormTextField';
+import { useFormContext } from '../../../../../providers/Form/hooks';
+import { FORM_MODES } from '../../../../../types/forms';
 import { CLUSTER_CREATION_FORM_NAMES } from '../../../names';
-import { ManageClusterSecretValues } from '../../../types';
+import { ManageClusterSecretDataContext, ManageClusterSecretValues } from '../../../types';
 
 export const ClusterName = () => {
     const {
@@ -11,6 +13,13 @@ export const ClusterName = () => {
         control,
         formState: { errors },
     } = useReactHookFormContext<ManageClusterSecretValues>();
+
+    const {
+        formData: { currentElement },
+    } = useFormContext<ManageClusterSecretDataContext>();
+
+    const isPlaceholder = typeof currentElement === 'string' && currentElement === 'placeholder';
+    const mode = isPlaceholder ? FORM_MODES.CREATE : FORM_MODES.EDIT;
 
     return (
         <Grid item xs={12}>
@@ -22,6 +31,7 @@ export const ClusterName = () => {
                 placeholder={'Enter cluster name'}
                 control={control}
                 errors={errors}
+                disabled={mode === FORM_MODES.EDIT}
             />
         </Grid>
     );
