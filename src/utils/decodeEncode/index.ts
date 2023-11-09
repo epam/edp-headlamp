ffd701dd01987eacbfc2f2b19c401d05a1719054
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2023-11-09 16:15:38 +0200

    fix: Fix bug with encoding already encoded values (#96)
    Jira: EPMDPEDP-12905
    Related: #96
    Change-Id: I10980ed2ea4dec1893391549fba604012809d5d9

--- src/utils/decodeEncode/index.ts
+++ src/utils/decodeEncode/index.ts
@@ -1,3 +1,5 @@
+import { isBase64String } from '../checks/isBase64String';
+
 export const safeDecode = (value: string, defaultValue: string = undefined) => {
     try {
         return value ? atob(unescape(value)) : defaultValue;
@@ -7,6 +9,9 @@ export const safeDecode = (value: string, defaultValue: string = undefined) => {
 };
 
 export const safeEncode = (value: string) => {
+    if (isBase64String(value)) {
+        return value;
+    }
     try {
         return btoa(unescape(value));
     } catch (e) {
