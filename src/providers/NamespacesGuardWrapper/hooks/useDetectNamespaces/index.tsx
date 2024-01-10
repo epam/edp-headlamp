4d16340e31ed8be693718b65e1c5c062e6d586b0
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-01-10 13:59:14 +0200

    fix: Fix hiding namespace notification after setting namespaces (#129)
    Jira: EPMDPEDP-13041
    Related: #129
    Change-Id: I87919ff9ce7561674e6b8ed1225bb22d4a8ba081

--- src/providers/NamespacesGuardWrapper/hooks/useDetectNamespaces/index.tsx
+++ src/providers/NamespacesGuardWrapper/hooks/useDetectNamespaces/index.tsx
@@ -3,7 +3,7 @@ import { Router, Utils } from '@kinvolk/headlamp-plugin/lib';
 import { IconButton, Link, Typography } from '@material-ui/core';
 import { useSnackbar } from 'notistack';
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { ICONS } from '../../../../icons/iconify-icons-mapping';
 import { LOCAL_STORAGE_SERVICE } from '../../../../services/local-storage';
 import { useDialogContext } from '../../../Dialog/hooks';
@@ -15,43 +15,45 @@ const LOCAL_STORAGE_KEY = 'namespacesGuardDialogClosed';
 export const useDetectNamespaces = (setKey: React.Dispatch<React.SetStateAction<string>>) => {
     const { enqueueSnackbar, closeSnackbar } = useSnackbar();
     const history = useHistory();
+    const location = useLocation();
     const { setDialog } = useDialogContext();
 
     const onDialogClose = React.useCallback(() => {
         LOCAL_STORAGE_SERVICE.setItem(LOCAL_STORAGE_KEY, 'true');
     }, []);
 
-    const clusterName = Utils.getCluster();
-    const settingsRoute = Router.createRouteURL('settingsCluster', { cluster: clusterName });
-    const lsNamespaceItem = localStorage.getItem(`cluster_settings.${clusterName}`);
-    const parsedLsNamespaceItem = JSON.parse(lsNamespaceItem);
-
-    const defaultNamespaceIsSet = !!lsNamespaceItem && !!parsedLsNamespaceItem?.defaultNamespace;
-    const allowedNamespacesIsSet =
-        !!lsNamespaceItem && !!parsedLsNamespaceItem?.allowedNamespaces?.length;
-
-    const errorMessage = React.useMemo(() => {
-        if (!defaultNamespaceIsSet && !allowedNamespacesIsSet) {
-            return 'Default namespace and allowed namespaces are unset.';
-        }
-        if (!defaultNamespaceIsSet) {
-            return 'Default namespace is unset.';
-        }
-        if (!allowedNamespacesIsSet) {
-            return 'Allowed namespaces are unset.';
-        }
-        return false;
-    }, [defaultNamespaceIsSet, allowedNamespacesIsSet]);
-
     React.useEffect(() => {
+        const clusterName = Utils.getCluster();
+        const settingsRoute = Router.createRouteURL('settingsCluster', { cluster: clusterName });
+        const lsNamespaceItem = localStorage.getItem(`cluster_settings.${clusterName}`);
+        const parsedLsNamespaceItem = JSON.parse(lsNamespaceItem);
+
+        const defaultNamespaceIsSet =
+            !!lsNamespaceItem && !!parsedLsNamespaceItem?.defaultNamespace;
+        const allowedNamespacesIsSet =
+            !!lsNamespaceItem && !!parsedLsNamespaceItem?.allowedNamespaces?.length;
         const dialogIsClosedOnce = LOCAL_STORAGE_SERVICE.getItem(LOCAL_STORAGE_KEY) === 'true';
+
+        const errorMessage = (() => {
+            if (!defaultNamespaceIsSet && !allowedNamespacesIsSet) {
+                return 'Default namespace and allowed namespaces are unset.';
+            }
+            if (!defaultNamespaceIsSet) {
+                return 'Default namespace is unset.';
+            }
+            if (!allowedNamespacesIsSet) {
+                return 'Allowed namespaces are unset.';
+            }
+            return false;
+        })();
+
+        closeSnackbar();
 
         if (defaultNamespaceIsSet && allowedNamespacesIsSet) {
             return;
         }
 
         if (dialogIsClosedOnce) {
-            closeSnackbar();
             enqueueSnackbar(
                 <Typography>
                     <span>{errorMessage} Navigate to </span>
@@ -95,16 +97,5 @@ export const useDetectNamespaces = (setKey: React.Dispatch<React.SetStateAction<
             modalName: NAMESPACES_GUARD_DIALOG_NAME,
             forwardedProps,
         });
-    }, [
-        allowedNamespacesIsSet,
-        closeSnackbar,
-        defaultNamespaceIsSet,
-        enqueueSnackbar,
-        errorMessage,
-        history,
-        onDialogClose,
-        setDialog,
-        setKey,
-        settingsRoute,
-    ]);
+    }, [location, closeSnackbar, enqueueSnackbar, history, onDialogClose, setDialog, setKey]);
 };
