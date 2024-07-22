4a4d51a8b91653c9170e3ae1ca5335c76aa72b2e
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-07-22 15:52:12 +0300

    feat: Add pull request link (#304)

---
+++ src/k8s/PipelineRun/utils/getPullRequestURL/index.test.ts
@@ -0,0 +1,84 @@
+import { PipelineKubeObjectInterface } from '../../../Pipeline/types';
+import { getPullRequestURL } from '.';
+
+describe('testing getPullRequestURL function', () => {
+  it('should return the correct URL for GitHub', () => {
+    const pipelineRun = {
+      spec: {
+        params: [
+          { name: 'git-source-url', value: 'git@github.com:user/test.git' },
+          { name: 'changeNumber', value: '123' },
+        ],
+      },
+      metadata: {
+        labels: {
+          'triggers.tekton.dev/trigger': 'github-trigger',
+        },
+      },
+    };
+
+    const result = getPullRequestURL(pipelineRun as unknown as PipelineKubeObjectInterface);
+
+    expect(result).toBe('https://github.com/user/test/pull/123');
+  });
+
+  it('should return the correct URL for GitLab', () => {
+    const pipelineRun = {
+      spec: {
+        params: [
+          { name: 'git-source-url', value: 'git@my-gitlab-provider.com:user/test.git' },
+          { name: 'changeNumber', value: '456' },
+        ],
+      },
+      metadata: {
+        labels: {
+          'triggers.tekton.dev/trigger': 'gitlab-review',
+        },
+      },
+    };
+
+    const result = getPullRequestURL(pipelineRun as unknown as PipelineKubeObjectInterface);
+
+    expect(result).toBe('https://my-gitlab-provider.com/user/test/merge_requests/456');
+  });
+
+  it('should return null if gitSourceUrl or changeNumber is missing', () => {
+    const pipelineRun = {
+      spec: {
+        params: [
+          { name: 'git-source-url', value: 'git@github.com:user/test.git' },
+          // changeNumber is missing
+        ],
+      },
+      metadata: {
+        labels: {
+          'triggers.tekton.dev/trigger': 'github-trigger',
+        },
+      },
+    };
+
+    const result = getPullRequestURL(pipelineRun as unknown as PipelineKubeObjectInterface);
+
+    expect(result).toBeNull();
+  });
+
+  it('should return null if gitSourceUrl includes "edp-ci"', () => {
+    const pipelineRun = {
+      spec: {
+        params: [
+          { name: 'git-source-url', value: 'https://example.com/edp-ci/repo.git' },
+          { name: 'changeNumber', value: '789' },
+        ],
+      },
+      metadata: {
+        labels: {
+          'triggers.tekton.dev/trigger': 'github-trigger',
+        },
+      },
+    };
+
+    const result = getPullRequestURL(pipelineRun as unknown as PipelineKubeObjectInterface);
+
+    expect(result).toBeNull();
+  });
+});
