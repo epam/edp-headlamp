68205680fb2348db88f5f8bc2df970522011ffeb
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-09-19 16:02:37 +0300

    feat: Add description column and validation (#386)

--- src/widgets/dialogs/ManageCDPipeline/components/fields/PipelineName/index.tsx
+++ src/widgets/dialogs/ManageCDPipeline/components/fields/PipelineName/index.tsx
@@ -3,6 +3,8 @@ import { FormTextField } from '../../../../../../providers/Form/components/FormT
 import { useTypedFormContext } from '../../../hooks/useFormContext';
 import { CDPIPELINE_FORM_NAMES } from '../../../names';
 
+const nameRequirementLabel = `Name must be not less than two characters long. It must contain only lowercase letters, numbers, and dashes. It cannot start or end with a dash, and cannot have whitespaces`;
+
 export const PipelineName = () => {
   const {
     register,
@@ -14,6 +16,11 @@ export const PipelineName = () => {
     <FormTextField
       {...register(CDPIPELINE_FORM_NAMES.name.name, {
         required: `The pipeline name must be at least 2 characters long and can only include lowercase letters, numbers, and dashes. It should not start or end with a dash or dot. E.g. my-java-application-1`,
+        pattern: {
+          value: /^[a-z](?!.*--[^-])[a-z0-9-]*[a-z0-9]$/,
+          message: nameRequirementLabel,
+        },
+        maxLength: { value: 15, message: 'Name must be not more than 15 characters long' },
       })}
       label={'Pipeline name'}
       title={'Enter a unique and descriptive name for your deployment pipeline.'}
