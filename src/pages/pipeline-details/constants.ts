0b476b2cef335bfb4d06dfbc31ee240233452d72
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-11-04 15:02:04 +0200

    feat: Add permission check for approve button (#456)

--- src/pages/pipeline-details/constants.ts
+++ src/pages/pipeline-details/constants.ts
@@ -1,3 +1,5 @@
+import { ApprovalTaskKubeObject } from '../../k8s/groups/EDP/ApprovalTask';
+import { ApprovalTaskKubeObjectConfig } from '../../k8s/groups/EDP/ApprovalTask/config';
 import { PipelineRunKubeObject } from '../../k8s/groups/Tekton/PipelineRun';
 import { PipelineRunKubeObjectConfig } from '../../k8s/groups/Tekton/PipelineRun/config';
 
@@ -7,6 +9,9 @@ export const permissionChecks = {
 
 export const permissionsToCheckConfig = {
   create: [{ instance: PipelineRunKubeObject, config: PipelineRunKubeObjectConfig }],
-  update: [{ instance: PipelineRunKubeObject, config: PipelineRunKubeObjectConfig }],
+  update: [
+    { instance: PipelineRunKubeObject, config: PipelineRunKubeObjectConfig },
+    { instance: ApprovalTaskKubeObject, config: ApprovalTaskKubeObjectConfig },
+  ],
   delete: [{ instance: PipelineRunKubeObject, config: PipelineRunKubeObjectConfig }],
 };
