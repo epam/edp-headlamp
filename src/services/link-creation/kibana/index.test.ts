7e435d1e4845b23fb9e2e9377018b15c3970e539
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-11-08 17:12:09 +0200

    fix: Kibana link (#473)

--- src/services/link-creation/kibana/index.test.ts
+++ src/services/link-creation/kibana/index.test.ts
@@ -5,7 +5,7 @@ describe('testing link-creation KibanaURLService', () => {
     expect(
       KibanaURLService.createDashboardLink('https://kibana-test.com', 'test-namespace')
     ).toEqual(
-      "https://kibana-test.com/app/discover#/?_g=()&_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes_namespace_name,negate:!f,params:(query:test-namespace),type:phrase),query:(match_phrase:(kubernetes_namespace_name:test-namespace)))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))"
+      "https://kibana-test.com/app/discover#/?_g=()&_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes.namespace_name,negate:!f,params:(query:test-namespace),type:phrase),query:(match_phrase:(kubernetes.namespace_name:test-namespace)))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))"
     );
   });
 });
