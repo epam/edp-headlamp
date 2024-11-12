531d66a0a791a407730594156e9cc7b4dc346338
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-11-12 11:18:42 +0200

    fix: Update kibana link (#473)

--- src/services/link-creation/kibana/index.ts
+++ src/services/link-creation/kibana/index.ts
@@ -4,6 +4,6 @@ export const KibanaURLService = {
       return undefined;
     }
 
-    return `${kibanaURLOrigin}/app/discover#/?_g=()&_a=(columns:!(message),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes.namespace_name,negate:!f,params:(query:${namespace}),type:phrase),query:(match_phrase:(kubernetes.namespace_name:${namespace})))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))`;
+    return `${kibanaURLOrigin}/app/discover#/?_g=()&_a=(columns:!(log),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:kubernetes.namespace_name,negate:!f,params:(query:${namespace}),type:phrase),query:(match_phrase:(kubernetes.namespace_name:${namespace})))),interval:auto,query:(language:kuery,query:''),sort:!(!('@timestamp',desc)))`;
   },
 };
