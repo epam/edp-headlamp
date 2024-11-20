ec015d4de23ffe82c5d77bf97c578cb2c41dc461
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-11-20 17:32:11 +0200

    feat: Hide manual qg name field (#496)

--- src/widgets/dialogs/ManageStage/components/fields/QualityGates/constants.ts
+++ src/widgets/dialogs/ManageStage/components/fields/QualityGates/constants.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const DEFAULT_QUALITY_GATE = {
   id: uuidv4(),
   qualityGateType: 'manual',
-  stepName: '',
+  stepName: 'approve',
   autotestName: null,
   branchName: null,
 };
