db7ae1952b13d3a33e21b70c85c004f595bc82a7
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2025-03-21 13:57:26 +0200

    feat: Add cluster secret status (#686)

--- src/k8s/groups/default/Secret/annotations.ts
+++ src/k8s/groups/default/Secret/annotations.ts
@@ -2,3 +2,6 @@ export const SECRET_ANNOTATION_INTEGRATION_SECRET_CONNECTED =
   'app.edp.epam.com/integration-secret-connected';
 export const SECRET_ANNOTATION_INTEGRATION_SECRET_ERROR =
   'app.edp.epam.com/integration-secret-error';
+
+export const SECRET_ANNOTATION_CLUSTER_CONNECTED = 'app.edp.epam.com/cluster-connected';
+export const SECRET_ANNOTATION_CLUSTER_ERROR = 'app.edp.epam.com/cluster-error';
