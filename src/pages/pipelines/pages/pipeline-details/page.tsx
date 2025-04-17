d5aae0d69a9fa4f1d45018eaf41bdf2ebfb2e8e3
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2025-04-17 10:50:26 +0300

    fix: Pipeline page filter context (#738)

--- src/pages/pipelines/pages/pipeline-details/page.tsx
+++ src/pages/pipelines/pages/pipeline-details/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { PageLogicWrapper } from '../../../../components/PageLogicWrapper';
 import { DialogContextProvider } from '../../../../providers/Dialog/provider';
+import { searchFunction } from '../../../../providers/Filter/constants';
+import { FilterContextProvider } from '../../../../providers/Filter/provider';
 import { PermissionsContextProvider } from '../../../../providers/Permissions/provider';
 import { TabsContextProvider } from '../../../../providers/Tabs/provider';
+import { getDefaultNamespace } from '../../../../utils/getDefaultNamespace';
 import { permissionsToCheckConfig } from './constants';
 import { PageView } from './view';
 
@@ -11,9 +14,17 @@ export default function () {
     <PageLogicWrapper>
       <PermissionsContextProvider permissionConfigs={permissionsToCheckConfig}>
         <DialogContextProvider>
-          <TabsContextProvider initialTabIdx={0} rememberLastTab id="pipeline-details-page">
-            <PageView />
-          </TabsContextProvider>
+          <FilterContextProvider
+            entityID={`PIPELINE_RUN_LIST_OVERVIEW::${getDefaultNamespace()}`}
+            matchFunctions={{
+              search: searchFunction,
+            }}
+            saveToLocalStorage
+          >
+            <TabsContextProvider initialTabIdx={0} rememberLastTab id="pipeline-details-page">
+              <PageView />
+            </TabsContextProvider>
+          </FilterContextProvider>
         </DialogContextProvider>
       </PermissionsContextProvider>
     </PageLogicWrapper>
