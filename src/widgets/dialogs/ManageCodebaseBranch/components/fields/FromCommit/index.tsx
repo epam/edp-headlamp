2cd25564026de1f7aa9e9da092e5a7f716cdc389
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2025-07-21 12:08:16 +0300

    fix: Fromcommit field free solo mode (#791)

--- src/widgets/dialogs/ManageCodebaseBranch/components/fields/FromCommit/index.tsx
+++ src/widgets/dialogs/ManageCodebaseBranch/components/fields/FromCommit/index.tsx
@@ -111,6 +111,7 @@ export const FromCommit = () => {
           }}
           AutocompleteProps={{
             loading: query.isLoading,
+            freeSolo: true,
           }}
         />
       );
